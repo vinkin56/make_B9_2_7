3e6e51b0dfaa2c092a17347b7b62ca6fa086cb90
vinkin56 <vinkin56@gmail.com>
2023-04-20 19:09:18 +0500

    Add composer

---
+++ array.js
@@ -0,0 +1,6 @@
+var msgArray = [];
+msgArray[0] = 'Привет';
+msgArray[99] = 'мир';
+if (msgArray.length === 100) {
+  console.log('Длина равна 100.');
+}
---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f8c0e887bdee3f4a17d0b808e3bb75a6ab25fc6c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f8c0e887bdee3f4a17d0b808e3bb75a6ab25fc6c',
+            'dev_requirement' => false,
+        ),
+        'swiftmailer/swiftmailer' => array(
+            'pretty_version' => 'v5.4.12',
+            'version' => '5.4.12.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../swiftmailer/swiftmailer',
+            'aliases' => array(),
+            'reference' => '181b89f18a90f8925ef805f950d47a7190e9b950',
+            'dev_requirement' => false,
+        ),
+    ),
+);
